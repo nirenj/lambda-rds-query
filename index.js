aeb09e2ddc431322268a7f38bda1ea5559fad845
Raymond Mouton <raymond_mouton@icloud.com>
2017-10-24 22:17:49 -0600

    adding support for large query results, and result paging in a non-breaking fashion

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+query_paged_results_cache = {};
+
 exports.handler = (event, context, callback) => {
   var mysql = require('mysql');
   var sizeof = require('object-sizeof');
 
   var DBPasswd = process.env.DB_PASSWD;
+
+  var large_query_result_expected = false;
+  var large_query_hash = null;
+  var large_query_result_page = null;
+  var do_response_size = false;
 
   if (event.keepalive) {
     callback(null,{'alive':true});
@@ -14,10 +21,34 @@ exports.handler = (event, context, callback) => {
       database : process.env.DB_NAME
     });
     if (event.query) {
-      var do_response_size = 0;
-      if (event.return_size) {
-          do_response_size = 1
+      if (event.lqre) {
+        large_query_result_expected = true;
+        if (event.next_result_page) {
+          large_query_result_page = event.next_result_page;
+        } else {
+          large_query_result_page = 0
+        }
+        if (event.query_hash) {
+          large_query_hash = event.query_hash;
+        } else {
+          // CALCULATE QUERY HASH
+        }
       }
+
+      if (event.return_size) {
+        do_response_size = true;
+        large_query_result_expected = false;
+        large_query_result_page = null;
+      }
+
+      if ((large_query_result_page != null) && (large_query_hash in query_paged_results_cache)) {
+        var large_query_result_next_page = null;
+        if ((large_query_result_page + 1) < query_paged_results_cache[large_query_hash].length ) {
+          large_query_result_next_page = (large_query_result_page + 1);
+        }
+        callback(null,{'results': query_paged_results_cache[large_query_hash][large_query_result_page], 'next_result_page': large_query_result_next_page,'query': event.query, 'query_hash': large_query_hash,'lqre': true});
+      }
+    
       db_connection.connect();
       db_connection.query({
         sql: event.query,
@@ -28,10 +59,15 @@ exports.handler = (event, context, callback) => {
           console.log('query error: ' + event.query);
           callback(error,null);
         } else {
-          if (do_response_size == 1) {
-            callback(null,{'bytes':sizeof(results)});
+          if (large_query_result_expected) {
+          // PAGE RESULT AND CACHE
+          // RETURN PAGE 
           } else {
-            callback(null,results);
+            if (do_response_size == 1) {
+              callback(null,{'bytes':sizeof(results)});
+            } else {
+              callback(null,results);
+            }
           }
         }
       });  
